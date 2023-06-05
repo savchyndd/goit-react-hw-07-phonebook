2b09488a43cb92f46454f859a1d8f03aace1c973
Dmytro <savchyndd@gmail.com>
2023-06-05 14:12:49 +0200

    add first files

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export const App = () => {
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
+  );
+};
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import { getContactsFilter, getContactsList } from 'redux/selectors';
+
+import { ContactsListItem } from '../ContactListItem/ContactListItem';
+
+import { ContactsList } from './ContactList.module';
+
+export const ContactList = () => {
+  const contacts = useSelector(getContactsList);
+  const filter = useSelector(getContactsFilter);
+  const visibleContacts = [
+    ...contacts.filter(contact => contact.name.toLowerCase().includes(filter)),
+  ];
+
+  return (
+    <ContactsList>
+      {visibleContacts.map(({ name, number, id }) => (
+        <ContactsListItem key={id} id={id} name={name} number={number} />
+      ))}
+    </ContactsList>
+  );
+};
