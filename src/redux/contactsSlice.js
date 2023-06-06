5fd9240b41878a51b3c88d5a00730bf01828d86e
Dmytro <savchyndd@gmail.com>
2023-06-06 09:38:33 +0200

    fix action deleted contact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -38,7 +38,9 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(task => task.id === action.payload);
+      const index = state.items.findIndex(
+        task => task.id === action.payload.id
+      );
       state.items.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
